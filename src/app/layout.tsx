40109dd40c43167bf41ccc47b26db48124d913a7
Jeff Domingos <jeffdomingos@example.com>
2026-03-04 18:04:03 -0300

    Force light mode by default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,8 +22,7 @@ export default function RootLayout({
             <body>
                 <ThemeProvider
                     attribute="class"
-                    defaultTheme="system"
-                    enableSystem
+                    defaultTheme="light"
                     disableTransitionOnChange
                 >
                     {children}
