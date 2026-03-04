be76f38ecff906540045391721acd21c976b2aa3
Jeff Domingos <jeffdomingos@example.com>
2026-03-04 18:25:17 -0300

    Restore base_url for backend and use wildcard targetOrigin

--- src/app/api/callback/route.ts
+++ src/app/api/callback/route.ts
@@ -48,7 +48,7 @@ export async function GET(request: Request) {
                     if (window.opener) {
                         window.opener.postMessage(
                             "authorization:github:success:{\\"token\\":\\"\${accessToken}\\",\\"provider\\":\\"github\\"}",
-                            window.location.origin
+                            "*"
                         );
                     }
                 </script>
